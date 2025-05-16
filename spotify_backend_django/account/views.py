694a18ef2eec69144f3dc864a5d83059e5022389
Akil Shaikh <140322467+Akil-Shaikh@users.noreply.github.com>
2025-05-16 14:07:54 +0000

    login_modules-Akil

---
+++ spotify_backend_django/account/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+# Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if User.objects.filter(email=email).exists():
+        return Response({"error": "Email already registered"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+    )
+    user.set_password(password)
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({"success": "Registered successfully", "token": token.key},status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"success": "Logged out successfully"},status=status.HTTP_200_OK)
+
+
